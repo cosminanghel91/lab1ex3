ca50054598c804d40e316b07e7494741aed6a079
Cosmin Anghel <cosmin.anghel@metaplast.ro>
2024-02-19 15:43:51 +0200

    Add project files.

---
+++ lab1ex3/Program.cs
@@ -0,0 +1,9 @@
+﻿/*
+* Scrieti un program care va afisa ultima cifra a unui numar intreg citit de la tastatura
+*/
+
+Console.WriteLine("Va rog introduceti numarul intreg pentru care doriti sa afisati ultima cifra");
+int numar = int.Parse(Console.ReadLine());
+
+int lastDigit = numar % (10);
+Console.WriteLine("Ultima cifra din numarul " + numar + " este: " + lastDigit);
